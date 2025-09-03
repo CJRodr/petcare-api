1edca65058d0f39d1d8e5f31fe125ec1797bc944
Carolina Rodriguez <carolinarodriguez@Carolinas-MacBook-Pro.local>
2025-09-02 20:08:42 -0600

    chore(db): add EF migration for owners and appointments

---
+++ Migrations/20250903013700_AddOwnerAndAppointment.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace petcare_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOwnerAndAppointment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
